cf59ed54bd413824a27c509247d9e62815ddd2cb
whereismyjetpack <djb44@psu.edu>
2022-11-24 20:54:29 -0500

    adds more fields to the monitor

--- src/config.py
+++ src/config.py
@@ -1,10 +1,7 @@
 import os
 
 
-class KumaConfig():
-  url = os.environ.get('UPTIME_KUMA_URL')
-  username = os.environ.get('UPTIME_KUMA_USERNAME')
-  password = os.environ.get('UPTIME_KUMA_PASSWORD')
-
-
-
+class KumaConfig:
+    url = os.environ.get("UPTIME_KUMA_URL")
+    username = os.environ.get("UPTIME_KUMA_USERNAME")
+    password = os.environ.get("UPTIME_KUMA_PASSWORD")
--- src/kuma_controller.py
+++ src/kuma_controller.py
@@ -2,31 +2,67 @@ import kopf
 import logging
 from uptime_kuma_api import UptimeKumaApi, MonitorType
 from config import KumaConfig
+import sys
 
-@kopf.on.delete('uptimekumahttpmonitors')
+
+@kopf.on.delete("uptimekumahttpmonitors")
 def delete_fn(body, **kwargs):
-    monitor_id = body.status.get('create_fn').get('monitorID')
     api = UptimeKumaApi(KumaConfig.url)
     api.login(KumaConfig.username, KumaConfig.password)
+    monitor_id = body.status.get("create_fn", {}).get("monitorID")
     result = api.delete_monitor(monitor_id)
     return result
 
 
-@kopf.on.create('uptimekumahttpmonitors')
+@kopf.on.create("uptimekumahttpmonitors")
 def create_fn(body, **kwargs):
-    spec = body.get('spec')
-    url = spec.get('url')
-    interval = spec.get('interval')
-    name = spec.get('name', body.get('metadata').get('name'))
-    
-
     api = UptimeKumaApi(KumaConfig.url)
     api.login(KumaConfig.username, KumaConfig.password)
-    result = api.add_monitor(
-        type=MonitorType.HTTP,
-        name=name,
-        url=url,
-        interval=interval,
-    )
+    notification_ids = []
+    notifications = api.get_notifications()
+    spec = body.get("spec")
 
+    request_body = {
+        "type": MonitorType.HTTP,
+        "name": spec.get("name", body.get("metadata").get("name")),
+        "accepted_statuscodes": str(spec.get('accepted_statuscodes')),
+        "interval": spec.get('interval'),
+        "maxretries": spec.get('maxretries'),
+        "retryInterval": spec.get('retryInterval'),
+        "expiryNotification": spec.get('expiryNotification'),
+        "url": spec.get('url'),
+        "ignoreTls": spec.get('ignoreTls'),
+        "method": spec.get('method'),
+        "body": spec.get('body'),
+        "headers": spec.get('headers')
+    }
+
+    if spec.get("defaultNotifiers"):
+        default_notifiers = [n["id"] for n in notifications if n["isDefault"]]
+        notification_ids.extend(default_notifiers)
+
+    if spec.get("notifiers"):
+        notifiers = [
+            n["id"]
+            for n in api.get_notifications()
+            if n["name"] in spec.get("notifiers")
+        ]
+        if not notifiers:
+            logging.warn(f"notifiers {spec.get('notifiers')} not found")
+        notification_ids.extend(notifiers)
+
+    if not notification_ids:
+        logging.warn(
+            f"notification list is empty. there will be no notifications for this monitor"
+        )
+
+    request_body["notificationIDList"] = notification_ids
+
+    result = api.add_monitor(**request_body)
     return result
+
+
+@kopf.on.cleanup()
+async def cleanup_fn(logger, **kwargs):
+    sys.exit(0)
+    pass
