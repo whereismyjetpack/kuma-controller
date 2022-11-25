d3b8f0083ead442c14a43faecf399b2769fb7690
whereismyjetpack <djb44@psu.edu>
2022-11-25 18:21:16 -0500

    also update existing monitors

--- src/kuma_controller.py
+++ src/kuma_controller.py
@@ -3,6 +3,7 @@ import logging
 from uptime_kuma_api import UptimeKumaApi, MonitorType
 from config import KumaConfig
 import sys
+from monitors import monitor_data
 
 
 @kopf.on.delete("uptimekumahttpmonitors")
@@ -14,59 +15,19 @@ def delete_fn(body, **kwargs):
     return result
 
 
+@kopf.on.update("uptimekumahttpmonitors")
 @kopf.on.create("uptimekumahttpmonitors")
 def create_fn(body, **kwargs):
     api = UptimeKumaApi(KumaConfig.url)
     api.login(KumaConfig.username, KumaConfig.password)
-    notification_ids = []
-    notifications = api.get_notifications()
-    spec = body.get("spec")
-    if spec.get('keyword'):
-        monitor_type = MonitorType.KEYWORD
+    monitor_id = body.status.get("create_fn", {}).get("monitorID")
+    request_body = monitor_data(body)
+
+    if monitor_id:
+        result = api.edit_monitor(monitor_id, **request_body)
     else:
-        monitor_type = MonitorType.HTTP
+        result = api.add_monitor(**request_body)
 
-    request_body = {
-        "type": monitor_type,
-        "name": spec.get("name", body.get("metadata").get("name")),
-        "accepted_statuscodes": [str(s) for s in spec.get('accepted_statuscodes')],
-        "interval": spec.get('interval'),
-        "upsideDown": spec.get('upsideDown'),
-        "resendInterval": spec.get('resendInterval'),
-        "keyword": spec.get('keyword'),
-        "maxretries": spec.get('maxretries'),
-        "maxredirects": spec.get('maxredirects'),
-        "retryInterval": spec.get('retryInterval'),
-        "expiryNotification": spec.get('expiryNotification'),
-        "url": spec.get('url'),
-        "ignoreTls": spec.get('ignoreTls'),
-        "method": spec.get('method'),
-        "body": spec.get('body'),
-        "headers": spec.get('headers')
-    }
-
-    if spec.get("defaultNotifiers"):
-        default_notifiers = [n["id"] for n in notifications if n["isDefault"]]
-        notification_ids.extend(default_notifiers)
-
-    if spec.get("notifiers"):
-        notifiers = [
-            n["id"]
-            for n in api.get_notifications()
-            if n["name"] in spec.get("notifiers")
-        ]
-        if not notifiers:
-            logging.warn(f"notifiers {spec.get('notifiers')} not found")
-        notification_ids.extend(notifiers)
-
-    if not notification_ids:
-        logging.warn(
-            f"notification list is empty. there will be no notifications for this monitor"
-        )
-
-    request_body["notificationIDList"] = notification_ids
-
-    result = api.add_monitor(**request_body)
     return result
 
 
